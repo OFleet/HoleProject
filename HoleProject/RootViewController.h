a122d1c1f2cafad6d79018a0a3799d77f636be99
MrHiQ <MistrHiQ@GMail.com>
2012-08-10 18:22:10 +1100

    Пустой проект
    Не отчищенная болванка от проекта Кокос2Д

---
+++ HoleProject/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HoleProject
+//
+//  Created by Mr. HiQ on 10.08.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
